edc73ac14044cc269172e655f98e91e76f34e31c
Debora Silva <deboraro.61@gmail.com>
2022-11-06 17:16:05 -0300

    estudos sobre upload de arquivos

--- aula_upload/index.php
+++ aula_upload/index.php
@@ -36,6 +36,40 @@ $getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
 //   echo "limite não permitido";
 // }
 
+
+echo time() . mb_strstr("", ".");
+
+if ($_FILES && !empty($_FILES["file"]["name"])) {
+  $fileUpload = $_FILES["file"];
+  // var_dump($fileUpload);
+
+  $allowedTypes = [
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "application/pdf",
+  ];
+
+  $newFilename = time() . mb_strstr($fileUpload["name"], ".");
+
+  //mb_strstr remove tudo a partir da primeira ocorrencia que nesse caso é ponto e elimia as coisas a esquerda
+
+  if (in_array($fileUpload["type"], $allowedTypes)) {
+
+    if (move_uploaded_file($fileUpload["tmp_name"], __DIR__ . "/uploads/{$newFilename}")) {
+      echo "arquivo enviado com sucesso";
+    } else {
+      echo "erro inesperado";
+    }
+  } else {
+    echo "tipo de arquivo não permitido";
+  }
+} elseif ($getPost) {
+  echo 'Wooops, o tamanho do arquivo é muito grande! O máximo permitido é ' . ini_get("post_max_size");
+} else {
+  echo 'Selecione um arquivo antes de enviar!';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,6 +83,8 @@ $getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
 </head>
 
 <body style="background: #fbf8cc;">
+
+
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"></script>
