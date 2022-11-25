bac84416e4a4b7baf2e4e03cef0d0a41f38cc715
Debora Silva <deboraro.61@gmail.com>
2022-11-24 22:07:46 -0300

    carregamento automatico

---
+++ 4-aula_poo/4_4_carregamento_automatico/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require("../../topo.html");
+
+require __DIR__ . "/source/Loading/User.php";
+require __DIR__ . "/source/Loading/Company.php";
+require __DIR__ . "/source/Loading/Address.php";
+
+$user = new \Source\Loading\User();
+$address = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+
+var_dump($user, $address, $company);
