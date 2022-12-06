b3eb5afdb4286223f835142eb43d008bec7a0a7b
Debora Silva <deboraro.61@gmail.com>
2022-12-05 21:23:48 -0300

    estudos sobre associação de objetos

--- 4-aula_poo/4_7_relacionamentos_entre_objetos/index.php
+++ 4-aula_poo/4_7_relacionamentos_entre_objetos/index.php
@@ -4,6 +4,8 @@ require __DIR__ . "/vendor/autoload.php";
 
 $teste = new Source\Related\Company();
 
-$teste->bootCompany("teste", "Rua aqui");
+$address = new Source\Related\Address("Rua aqui", 23, "Bloco A");
 
-var_dump($teste);
+$teste->boot("Empresa", $address);
+
+var_dump($teste->getAddress()->getStreet());
--- 4-aula_poo/4_7_relacionamentos_entre_objetos/source/Related/Company.php
+++ 4-aula_poo/4_7_relacionamentos_entre_objetos/source/Related/Company.php
@@ -4,12 +4,24 @@ namespace Source\Related;
 
 class Company
 {
+  /**
+   * Undocumented variable
+   *A nossa variavel address está referenciando nosso objeto com o mesmo nome Address
+   * @var Address
+   */
   private $company;
   private $address;
   private $team;
   private $products;
 
   public function bootCompany($company, $address)
+  {
+    $this->company = $company;
+    $this->address = $address;
+  }
+
+
+  public function boot($company, Address $address)
   {
     $this->company = $company;
     $this->address = $address;
