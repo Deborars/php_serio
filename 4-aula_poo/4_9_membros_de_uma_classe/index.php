e64630e80dfc8dfeb588c63bbbcfe3770b4e626b
Debora Silva <deboraro.61@gmail.com>
2023-01-03 18:16:49 -0300

    iniciando os estudos sobre propriedades de uma classe

---
+++ 4-aula_poo/4_9_membros_de_uma_classe/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("source/Members/Config.php");
+
+use Source\Members\Config;
+
+$config = new Config();
+
+var_dump($config);
---
+++ 4-aula_poo/4_9_membros_de_uma_classe/source/Members/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Source\Members;
+
+class Config
+{
+  const COMPANY = "Upside";
+  const DOMAIN = "Upside.com.br";
+  const SECTOR = "Educação";
+}
