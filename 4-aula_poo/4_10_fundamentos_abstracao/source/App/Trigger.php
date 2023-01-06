bafc2115fc45928d88e875176dcd3a6f7ec06f9e
Debora Silva <deboraro.61@gmail.com>
2023-01-06 16:29:23 -0300

    adicionando a classe trigger e iniciando a user

---
+++ 4-aula_poo/4_10_fundamentos_abstracao/source/App/Trigger.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Source\App;
+
+use Reflection;
+
+class Trigger
+{
+  private const TRIGGER =  "alert";
+  public const SUCCESS = 'alert-success';
+  public const WARNING = 'alert-warning';
+  public const DANGER = 'alert-danger';
+
+  private static $message;
+  private static $errorType;
+  private static $error;
+
+  private static function setError($message, $errorType)
+  {
+    $reflection = new \ReflectionClass(__CLASS__);
+    $errorTypes = $reflection->getConstants();
+
+    self::$message = $message;
+    //in_array vai verificar a partir do array das constantes sucess, warning...se uma delas foi passada
+    //no errortype
+    self::$errorType = (!empty($errorType) && in_array($errorType, $errorTypes) ? $errorType : "dark");
+    self::$error = "<p class='" . self::TRIGGER . " ALERT-" . self::$errorType . "'>" . self::$message . "</p>";
+  }
+
+  public static function show($message, $errorType = null)
+  {
+    self::setError($message, $errorType);
+    echo self::$error;
+  }
+
+  public static function push($message, $errorType = null)
+  {
+    self::setError($message, $errorType);
+    return self::$error;
+  }
+}
