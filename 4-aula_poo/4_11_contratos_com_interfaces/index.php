d68ce95453a2e9c2fef78b3c2c20bdb8824c63a0
Debora Silva <deboraro.61@gmail.com>
2023-01-11 09:57:00 -0300

    conclusão sobre contratos com interfaces

--- 4-aula_poo/4_11_contratos_com_interfaces/index.php
+++ 4-aula_poo/4_11_contratos_com_interfaces/index.php
@@ -2,7 +2,18 @@
 
 require_once("source/Contracts/UserInterface.php");
 require_once("source/Contracts/User.php");
+require_once("source/Contracts/UserErrorInterface.php");
+require_once("source/Contracts/UserAdmin.php");
+require_once("source/Contracts/Login.php");
 
 $user = new \Source\Contracts\User("Debora", "Silva", "teset@teste.com");
 
-var_dump($user);
+$admin = new \Source\Contracts\UserAdmin("Debora", "Silva", "teset@teste.com");
+
+// var_dump($user, $admin);
+
+$login = new \Source\Contracts\Login();
+
+$loginAdmin = $login->loginAdmin($admin);
+
+var_dump($loginAdmin);
