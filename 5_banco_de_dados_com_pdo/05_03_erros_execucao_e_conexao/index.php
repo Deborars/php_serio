a9392327a22e6a05dc96a6bf5a59f5ea273e04e8
Debora Silva <deboraro.61@gmail.com>
2023-01-18 15:05:42 -0300

    testando os metodos query e exec do PDO

--- 5_banco_de_dados_com_pdo/05_03_erros_execucao_e_conexao/index.php
+++ 5_banco_de_dados_com_pdo/05_03_erros_execucao_e_conexao/index.php
@@ -4,4 +4,12 @@ require_once("source/Database/Connect.php");
 
 use Source\Database\Connect;
 
-$conn = Connect::getInstance();
+$inset = "Insert into users (first_name, last_name, email, document) 
+  values ('Robson', 'Leite', 'curso@upside.com.br', '123245')";
+
+try {
+  $conn = Connect::getInstance()->exec($inset);
+  var_dump($conn);
+} catch (PDOException $exception) {
+  var_dump($exception);
+}
