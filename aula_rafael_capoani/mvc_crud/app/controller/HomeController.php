1fb471acdd7ce341757f30f9ba2693cf3aa54287
Debora Silva <deboraro.61@gmail.com>
2023-03-26 15:16:28 -0300

    aula 6 view com twig

--- aula_rafael_capoani/mvc_crud/app/controller/HomeController.php
+++ aula_rafael_capoani/mvc_crud/app/controller/HomeController.php
@@ -6,7 +6,15 @@ class HomeController
   {
     try {
       $colectPostagens = Postagem::selecionaTodos();
-      var_dump($colectPostagens);
+
+      $loader = new \Twig\Loader\FilesystemLoader('app/View');
+      $twig = new \Twig\Environment($loader);
+      $template = $twig->load('home.html');
+
+      $parametros['nome'] = 'Debora';
+
+      $conteudo = $template->render($parametros);
+      echo $conteudo;
     } catch (Exception $exception) {
       var_dump($exception->getMessage());
     }
