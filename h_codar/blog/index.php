f4d16b951abacc26239169b27720da46407ed409
Debora Silva <deboraro.61@gmail.com>
2023-04-23 16:39:23 -0300

    blog

---
+++ h_codar/blog/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+include("helpers/url.php");
+?>
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog</title>
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+
+<body>
+
+
+</body>
+
+</html>
