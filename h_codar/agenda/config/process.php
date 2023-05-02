d99bbdfa4eeb7778f8653df8d0d63346a24bbd28
Debora Silva <deboraro.61@gmail.com>
2023-05-01 21:24:16 -0300

    criado o componente de voltar

--- h_codar/agenda/config/process.php
+++ h_codar/agenda/config/process.php
@@ -4,12 +4,23 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$query = 'SELECT * FROM contacts';
+if (!empty($_GET['id'])) {
 
-$stmt = $conn->prepare($query);
-
-$stmt->execute();
-// By this way you can close connection in PDO.
-$conn = null;
-
-$contacts = $stmt->fetchAll();
+  // retorna de um dado
+  $id = $_GET['id'];
+  $query = 'SELECT * FROM contacts where id = :id';
+  $stmt = $conn->prepare($query);
+  $stmt->bindParam(":id", $id);
+  $stmt->execute();
+  // By this way you can close connection in PDO.
+  $conn = null;
+  $contact = $stmt->fetch();
+} else {
+  // retorna todos os contatos
+  $query = 'SELECT * FROM contacts';
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  // By this way you can close connection in PDO.
+  $conn = null;
+  $contacts = $stmt->fetchAll();
+}
