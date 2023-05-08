ac16111a3b9068ed97b169b57e37cb48c23ee48d
Debora Silva <deboraro.61@gmail.com>
2023-05-07 21:19:31 -0300

    dao finalizado

--- h_codar/dao/index.php
+++ h_codar/dao/index.php
@@ -1,2 +1,32 @@
 <?php
-  include_once("db.php");
+include_once("db.php");
+include_once("dao/CarDAO.php");
+
+$CarDAO = new CarDAO($conn);
+$cars = $CarDAO->findAll();
+?>
+
+<h1>Insira um carro:</h1>
+<form action="process.php" method="POST">
+  <div>
+    <label for="brand">Marca</label>
+    <input type="text" name="brand" id="brand" placeholder="Insira a marca">
+  </div>
+  <div>
+    <label for="brand">Km</label>
+    <input type="text" name="km" id="km" placeholder="Insira a kilometragem">
+  </div>
+  <div>
+    <label for="brand">Cor</label>
+    <input type="text" name="color" id="color" placeholder="Insira a cor">
+  </div>
+  <input type="submit" value="Salvar">
+</form>
+
+<ul>
+  <?php foreach ($cars as $car) { ?>
+  <li><?= $car->getBrand() ?> - <?= $car->getKm() ?> -
+    <?= $car->getColor() ?>
+  </li>
+  <?php } ?>
+</ul>
