2a06f8b8ad3314be4bd7ab5d9632ed6421135b4d
Debora Silva <deboraro.61@gmail.com>
2023-05-28 20:10:55 -0300

    css

--- h_codar/moviestar/auth.php
+++ h_codar/moviestar/auth.php
@@ -7,42 +7,44 @@
         <h2>Entrar</h2>
         <form action="" method="post">
           <input type="hidden" value="login" name="type">
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="email">E-mail:</label>
             <input type="email" class="form-control" placeholder="Digite seu e-mail" name="email" id="email">
           </div>
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="password">Senha:</label>
             <input type="password" class="form-control" placeholder="Digite sua senha" name="password" id="password">
           </div>
-          <input type="submit" class="btn card-btn" value="Entrar">
+          <input type="submit" class="btn card-btn mt-3" value="Entrar">
         </form>
       </div>
       <div class="col-md-4" id="register-container">
         <h2>Criar Conta</h2>
         <form action="" method="post">
           <input type="hidden" value="register" name="type">
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="email">E-mail:</label>
             <input type="email" class="form-control" placeholder="Digite seu e-mail" name="email" id="email">
           </div>
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="name">Nome:</label>
             <input type="text" class="form-control" placeholder="Digite seu nome" name="name" id="name">
           </div>
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="lastName">Sobrenome:</label>
             <input type="text" class="form-control" placeholder="Digite seu sobrenome" lastName="lastName" id="lastName">
           </div>
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="password">Senha:</label>
             <input type="password" class="form-control" placeholder="Digite sua senha" name="password" id="password">
           </div>
-          <div class="form-group">
+          <div class="form-group mt-2">
             <label for="confirmPassword">Confirmação de senha:</label>
             <input type="confirmPassword" class="form-control" placeholder="Confirme sua senha" name="confirmPassword" id="confirmPassword">
           </div>
-          <input type="submit" class="btn card-btn" value="Registrar">
+          <div class="form-group mt-3">
+            <input type="submit" class=" btn card-btn" value="Registrar">
+          </div>
         </form>
       </div>
     </div>
