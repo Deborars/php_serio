a05d2c8bd010e7bf341efa8cd58335b0cc603dc6
Debora Silva <deboraro.61@gmail.com>
2023-05-28 20:03:38 -0300

    estrutura de login e registro de usuarios

--- h_codar/moviestar/auth.php
+++ h_codar/moviestar/auth.php
@@ -1,7 +1,52 @@
 <?php require('templates/header.php') ?>
 
 <div id="main-container" class="container-fluid">
-  <h1>Autenticação</h1>
+  <div class="col-md-12">
+    <div class="row" id="auth-row">
+      <div class="col-md-4" id="login-container">
+        <h2>Entrar</h2>
+        <form action="" method="post">
+          <input type="hidden" value="login" name="type">
+          <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" class="form-control" placeholder="Digite seu e-mail" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="password">Senha:</label>
+            <input type="password" class="form-control" placeholder="Digite sua senha" name="password" id="password">
+          </div>
+          <input type="submit" class="btn card-btn" value="Entrar">
+        </form>
+      </div>
+      <div class="col-md-4" id="register-container">
+        <h2>Criar Conta</h2>
+        <form action="" method="post">
+          <input type="hidden" value="register" name="type">
+          <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" class="form-control" placeholder="Digite seu e-mail" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="name">Nome:</label>
+            <input type="text" class="form-control" placeholder="Digite seu nome" name="name" id="name">
+          </div>
+          <div class="form-group">
+            <label for="lastName">Sobrenome:</label>
+            <input type="text" class="form-control" placeholder="Digite seu sobrenome" lastName="lastName" id="lastName">
+          </div>
+          <div class="form-group">
+            <label for="password">Senha:</label>
+            <input type="password" class="form-control" placeholder="Digite sua senha" name="password" id="password">
+          </div>
+          <div class="form-group">
+            <label for="confirmPassword">Confirmação de senha:</label>
+            <input type="confirmPassword" class="form-control" placeholder="Confirme sua senha" name="confirmPassword" id="confirmPassword">
+          </div>
+          <input type="submit" class="btn card-btn" value="Registrar">
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php require('templates/footer.php') ?>
--- h_codar/moviestar/templates/header.php
+++ h_codar/moviestar/templates/header.php
@@ -14,13 +14,9 @@ $flassMessage = [];
   <title>MovieStar</title>
   <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico" type="image/x-icon">
   <!-- bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css"
-    integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css" integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!--font awesome  -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!--css  -->
   <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
 </head>
@@ -32,13 +28,11 @@ $flassMessage = [];
         <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
         <span id="moviestar-title">MovieStar</span>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-        aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
       </button>
       <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes"
-          aria-label="Search">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
         <button class="btn my-2 my-sm-0" type="submit">
           <i class="fas fa-search"></i>
         </button>
@@ -53,9 +47,7 @@ $flassMessage = [];
     </nav>
   </header>
   <?php if (!empty($flassMessage['msg'])) { ?>
-
+    <div class="msg-container">
+      <p class="msg <?= $flassMessage['type'] ?>"><?= $flassMessage['msg'] ?></p>
+    </div>
   <?php } ?>
-
-  <div class="msg-container">
-    <p class="msg <?=$flassMessage['type']?>"><?=$flassMessage['msg']?></p>
-  </div>
