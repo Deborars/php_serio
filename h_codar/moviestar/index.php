6d093ef18892c67f2b5f74e3a248bf27e8b7a519
Debora Silva <deboraro.61@gmail.com>
2023-05-28 16:54:05 -0300

    criando header e footer

--- h_codar/moviestar/index.php
+++ h_codar/moviestar/index.php
@@ -12,13 +12,9 @@ require_once("db.php");
   <title>MovieStar</title>
   <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico" type="image/x-icon">
   <!-- bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css"
-    integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css" integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!--font awesome  -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!--css  -->
   <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
 </head>
@@ -30,29 +26,55 @@ require_once("db.php");
         <img id="logo" src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar">
         <span id="moviestar-title">MovieStar</span>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-targer="#navbar" aria-controls="navbar"
-        aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-targer="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
       </button>
       <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes"
-          aria-label="Search">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
         <button class="btn my-2 my-sm-0" type="submit">
           <i class="fas fa-search"></i>
         </button>
       </form>
-      <div class="collapse navbar-collapse" id="navbar"></div>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
+  <div id="main-container" class="container-fluid">
+    <h1>Corpo do Site</h1>
+  </div>
+  <footer id="footer">
+    <div class="social-container">
+      <ul>
+        <li>
+          <a href="#"><i class="fab fa-facebook-square"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-youtube-square"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-instagram-square"></i></a>
+        </li>
+      </ul>
+    </div>
 
+    <div id="footer-link-container">
+      <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar crítica</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+      </ul>
+    </div>
 
+    <p>&copy; <?= date("Y"); ?> Deborars</p>
+  </footer>
   <!-- bootstrap -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.js"
-    integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
